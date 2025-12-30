036c38feb83e03ac22c0e71c852c5b39b0dd5ffd
fyanez <fyanez@cs.toronto.edu>
2025-12-30 13:06:33 -0500

    Revert "initial commit - new repo"
    This reverts commit 5aeabac75c8bd06fded17423e4fa30fea78a4f47.

--- src/components/domain/pension_fund/request-review/index.js
+++
@@ -1,8 +0,0 @@
-// Main components used in RequestReviewView
-export { default as RequestReviewHeader } from './RequestReviewHeader';
-export { default as RequestContextSection } from './RequestContextSection';
-export { default as ConflictsSection } from './ConflictsSection';
-export { default as ReviewSection } from './ReviewSection';
-
-// Supporting components
-export { default as ConflictCard } from './ConflictCard';
